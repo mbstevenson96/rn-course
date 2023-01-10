bcd58165c4febe1252996f2fef3d7286276fd1f0
mbstevenson96 <mbstevenson96@gmail.com>
2023-01-10 10:55:46 -0500

    add button

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello, World!</Text>
-      <StatusBar style="auto" />
+      <Text>Another piece of text</Text>
+      <Text>Hello World!</Text>
+      <Button title='Tap Me!' />
     </View>
   );
 }
