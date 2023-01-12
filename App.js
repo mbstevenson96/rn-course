6a82ce0e09129f3a50154d421282ad7f733335d4
mbstevenson96 <mbstevenson96@gmail.com>
2023-01-12 10:06:09 -0500

    set up handler functions for adding goals

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
+import { useState } from 'react';
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState('')
+
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText)
+  }
+
+  function addGoalHandler() {
+    console.log(enteredGoalText);
+  }
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder='Your Goal!' />
-        <Button title='Add Goal' />
+        <TextInput style={styles.textInput} placeholder='Your Goal!' onChangeText={goalInputHandler} />
+        <Button title='Add Goal' onPress={addGoalHandler} />
       </View>
       <View style={styles.goalsContainer}>
         <Text>List of goals...</Text>
