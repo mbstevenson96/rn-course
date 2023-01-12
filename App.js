abf72f34e3f34054a75e9d6f72ed556c83f94208
mbstevenson96 <mbstevenson96@gmail.com>
2023-01-12 10:14:36 -0500

    set up function for adding goals and set up .map to add goals

--- App.js
+++ App.js
@@ -3,13 +3,14 @@ import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   const [enteredGoalText, setEnteredGoalText] = useState('')
+  const [goals, setGoals] = useState([])
 
   function goalInputHandler(enteredText) {
     setEnteredGoalText(enteredText)
   }
 
   function addGoalHandler() {
-    console.log(enteredGoalText);
+    setGoals(currentCourseGoals => [...currentCourseGoals, enteredGoalText])
   }
 
   return (
@@ -19,7 +20,9 @@ export default function App() {
         <Button title='Add Goal' onPress={addGoalHandler} />
       </View>
       <View style={styles.goalsContainer}>
-        <Text>List of goals...</Text>
+        {goals.map((goal) => 
+          <Text key={goal}>{goal}</Text>
+        )}
       </View>
     </View>
   );
