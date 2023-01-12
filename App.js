1baa480e3924fdbe6a1e3bc2273bb593ebc40a37
mbstevenson96 <mbstevenson96@gmail.com>
2023-01-12 11:28:30 -0500

    update scroll feature to FlatList

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, ScrollView, FlatList } from 'react-native';
 
 export default function App() {
   const [enteredGoalText, setEnteredGoalText] = useState('')
@@ -10,7 +10,10 @@ export default function App() {
   }
 
   function addGoalHandler() {
-    setGoals(currentCourseGoals => [...currentCourseGoals, enteredGoalText])
+    setGoals(currentGoals => [
+      ...currentGoals,
+      {text: enteredGoalText, key: Math.random().toString()}
+    ])
   }
 
   return (
@@ -20,13 +23,17 @@ export default function App() {
         <Button title='Add Goal' onPress={addGoalHandler} />
       </View>
       <View style={styles.goalsContainer}>
-        <ScrollView alwaysBounceVertical={false}>
-          {goals.map((goal) => 
-            <View style={styles.goalItem} key={goal}>
-              <Text style={styles.goaldText}>{goal}</Text>
-            </View>
-          )}
-        </ScrollView>
+        <FlatList 
+          data={goals}
+          renderItem={(itemData) => {
+            return (
+              <View style={styles.goalItem}>
+                <Text style={styles.goaldText}>{itemData.item.text}</Text>
+              </View>
+            )
+          }} 
+          alwaysBounceVertical={false} 
+        />
       </View>
     </View>
   );
